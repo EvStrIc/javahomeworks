6c4a4bdca22d2ef4a37a9f5de5ba3f031aab727b
EvStrIc <strangeevan@gmail.com>
2022-04-29 16:43:30 +0300

    added say hello task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        HelloWorld write = new HelloWorld();
+        System.out.println(write.getHello());
     }
 }
