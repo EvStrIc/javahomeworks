ac07ed2133f41defd5ba4d42e772dcfa32254f45
EvStrIc <strangeevan@gmail.com>
2022-05-05 12:06:25 +0300

    added XOXO class for TicTacToe game. Experimenting with players' turns methods. Not working properly yet.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.tasks.Calculator;
 import com.company.tasks.HelloWorld;
 import com.company.tasks.Squares;
+import com.company.tasks.XOXO;
 
 public class Main {
 
@@ -10,11 +11,12 @@ public class Main {
         //HelloWorld write = new HelloWorld();
         //System.out.println(write.getHello());
 
-        Calculator operation = new Calculator();
-        System.out.println(operation.Calc());
+        //Calculator operation = new Calculator();
+        //System.out.println(operation.Calc());
 
-        Squares figureSqr = new Squares();
-        System.out.println(figureSqr.Sqr());
+        //Squares figureSqr = new Squares();
+        //System.out.println(figureSqr.Sqr());
 
+        new XOXO().game();
     }
 }
